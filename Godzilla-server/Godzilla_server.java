380a8ab9a93ee0fb92663cf99f9341ca7a095089
CamilloIsNotAvailable <91274274+CamilloIsNotAvailable@users.noreply.github.com>
2023-11-12 16:41:15 +0100

    Add files via upload

--- Godzilla-server/Godzilla_server.java
+++ Godzilla-server/Godzilla_server.java
@@ -1,14 +1,25 @@
+import gui.Server_frame;
 import network.Net_listener;
+import java.io.IOException;
 
 public class Godzilla_server {
     private static final int PORT = 31415;
 
     public static void main(String[] args) {
-        start_msg();
+//        Server_frame.init();
+        Runtime.getRuntime().addShutdownHook(new Thread(shutdown_hook));
         Net_listener.start(PORT);
     }
 
-    private static void start_msg() {
-        System.out.println("Godzilla_server: start");
-    }
+    private static Runnable shutdown_hook = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                Net_listener.save_credentials();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
 }
